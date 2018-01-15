f62192b145088396ca5ad9b4c5103eb7157bd525
Marc <fraysse.marc@gmail.com>
2018-01-15 09:59:15 +0100

    Added unvaccinated only study

--- src/VaccinesAndDiseasesOverYears.m
+++ src/VaccinesAndDiseasesOverYears.m
@@ -22,13 +22,37 @@ angolaVaccineCoverage = completeVaccineCoverage(4,:);
 % end
 
 %%Only diseased countries study
+% figure(1);
+% for i = 1:30
+%    for j = 1:17
+%        if completeTetanosCases(i,j)>100 
+%             plot(completeVaccineCoverage(i,:),completeTetanosCases(i,:),'sr','LineWidth',3);
+%             hold on;
+%        end
+%    end
+% end
+% 
+%%Only diseased unvaccinated countries study
 figure(1);
 for i = 1:30
    for j = 1:17
-       if completeTetanosCases(i,j)>100
-           plot(completeVaccineCoverage(i,:),completeTetanosCases(i,:),'sr','LineWidth',3);
-           hold on;
+       if completeTetanosCases(i,j)>100 
+           if completeVaccineCoverage(i,17) < 95
+               plot(completeVaccineCoverage(i,:),completeTetanosCases(i,:),'sr','LineWidth',3);
+               hold on;
+           end
        end
    end
 end
+
+%%Only unvaccianted countries study
+% figure(1);
+% for i = 1:30
+%    for j = 1:17
+%        if completeVaccineCoverage(i,17) < 95
+%            plot(completeVaccineCoverage(i,:),completeTetanosCases(i,:),'sr','LineWidth',3);
+%            hold on;
+%        end
+%    end
+% end
 
